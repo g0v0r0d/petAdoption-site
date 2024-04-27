291b2094255a31b9bf8db3d24d597149ed7e7bb3
qeeera <www.savitchgarik@yandex.ru>
2024-04-27 12:08:57 +0300

    Adopted

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Header } from './components/header';
+import { PetList } from './components/petList';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { PetDetails } from './components/petDetails';
+import { PetAdoptionForm } from './components/PetAdoptionForm';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<PetList></PetList>}></Route>
+          <Route path="/details/:id" element={<PetDetails />}></Route>
+          <Route path="/adoptions/:id" element={<PetAdoptionForm />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
